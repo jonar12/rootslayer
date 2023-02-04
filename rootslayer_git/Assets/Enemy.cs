4a522b6ad817cb07a4d8a716197a05574be0808e
Jonathan Arredondo <jona.arredondo12@gmail.com>
2023-02-04 09:52:59 -0600

    Enemigo ya no se pega al jugador

--- rootslayer_git/Assets/Enemy.cs
+++ rootslayer_git/Assets/Enemy.cs
@@ -7,6 +7,7 @@ public class Enemy : MonoBehaviour
     public GameObject jugador;
     public float velocidad;
     public float distancia;
+    public float distanciaDeteccion;
     public float distanciaMin;    
 
     void Start(){
@@ -19,7 +20,7 @@ public class Enemy : MonoBehaviour
         direccion.Normalize();
         float angulo = Mathf.Atan2(direccion.y, direccion.x) * Mathf.Rad2Deg;
 
-        if(distancia < distanciaMin) {
+        if(distancia < distanciaDeteccion && distancia > distanciaMin) {
             transform.position = Vector2.MoveTowards(this.transform.position, jugador.transform.position, velocidad * Time.deltaTime);
             transform.rotation = Quaternion.Euler(Vector3.forward * angulo);
         }
