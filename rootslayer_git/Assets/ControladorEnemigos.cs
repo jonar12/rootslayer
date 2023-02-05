13f3688f082f66e22966b3819a988276adf99ae2
Jonathan Arredondo <jona.arredondo12@gmail.com>
2023-02-04 23:36:40 -0600

    Desactivacion de spawner por tiempo y timer/display de ronda en UI añadidos

--- rootslayer_git/Assets/ControladorEnemigos.cs
+++ rootslayer_git/Assets/ControladorEnemigos.cs
@@ -14,7 +14,7 @@ public class ControladorEnemigos : MonoBehaviour
     [SerializeField] private int numEnemigos;
     [SerializeField] private int minNumEnemigos;
     [SerializeField] private int maxNumEnemigos;
-
+    private float timeToOff = 10f;
     private float tiempoSiguienteEnemigo;
 
     // Start is called before the first frame update
@@ -31,9 +31,12 @@ public class ControladorEnemigos : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (timeToOff > 0) {
+            timeToOff -= Time.deltaTime;
+        }
         tiempoSiguienteEnemigo += Time.deltaTime;
 
-        if(tiempoSiguienteEnemigo >= tiempoEnemigos) {
+        if(tiempoSiguienteEnemigo >= tiempoEnemigos && timeToOff > 0) {
             tiempoSiguienteEnemigo = 0;
             tiempoEnemigos = getTiempoEnemigosRandom(tiempoMinEnemigos, tiempoMaxEnemigos);
             // Crear enemigo
@@ -54,5 +57,9 @@ public class ControladorEnemigos : MonoBehaviour
 
     private float getTiempoEnemigosRandom(float min, float max) {
         return Random.Range(min,max);
+    }
+
+    public float getTimeToOff() {
+        return timeToOff;
     }
 }
