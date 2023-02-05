9607d50d02cb145eb58363095c8fa5934ad579f2
NivekZenun555 <124401871+NivekZenun555@users.noreply.github.com>
2023-02-04 21:05:29 -0600

    Implementa armas, control, rotación, animación, y daño

--- rootslayer_git/Assets/CambiarRotacion.cs
+++ rootslayer_git/Assets/CambiarRotacion.cs
@@ -6,8 +6,12 @@ public class CambiarRotacion : MonoBehaviour
 {
 	[SerializeField] private Camera camera2;
 	[Header("MovimientoCamara")]
-	[SerializeField] private Vector3 objetivo;
-
+		[SerializeField] private Vector3 objetivo;
+	
+	public Animator animator;
+	public float delay = 0.3f;
+	private bool attackBlocked;
+	
 	
 	private void Update()
 	{
@@ -17,5 +21,19 @@ public class CambiarRotacion : MonoBehaviour
 		float anguloGrados = (180/Mathf.PI) * anguloRadianes - 90;
 		transform.rotation = Quaternion.Euler(0, 0, anguloGrados);
 	}
+	
+	public void Attack()
+	{
+		if (attackBlocked)
+			return;
+		animator.SetTrigger("Attack");
+		attackBlocked = true;
+		StartCoroutine(DelayAttack());
+	}
+	
+	private IEnumerator DelayAttack()
+	{
+		yield return new WaitForSeconds(delay);
+		attackBlocked = false;
+	}
 }
-
